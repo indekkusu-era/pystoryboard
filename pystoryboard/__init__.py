1990db7f44a61dcb6384952896ada3b6cfbbadd1
indexerror <lkycst@gmail.com>
2023-08-20 00:15:14 +0800

    move everything to pysb folder

---
+++ pystoryboard/__init__.py
@@ -0,0 +1,5 @@
+from .objects import Sprite, Animation
+from .events import Scale, Move, Fade, Rotate, VectorScale, MoveX, MoveY, Color, Parameter
+from .events.event_classes import Vector, Color as RGB
+
+__all__ = ['Sprite', 'Animation', 'Scale', 'Move', 'Fade', 'Rotate', 'VectorScale', 'MoveX', 'MoveY', 'Color', 'Parameter', 'Vector', 'RGB']
